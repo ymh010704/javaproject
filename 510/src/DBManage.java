458637a972acef252a93cdecbb1815080196b43a
USER <USER@INET-10>
2018-05-10 18:55:34 +0900

    2018-05-10 / MySQL이랑 JAVA랑 연결한거

---
+++ 510/src/DBManage.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+import javax.swing.JOptionPane;
+
+public class DBManage {
+	public static Connection con;
+	public static Statement stmt;
+	public static ResultSet rs;
+	
+	public void DBSetting(){
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost/?serverTimezone=UTC","root","1234");
+			stmt = con.createStatement();
+			System.out.println("DB접속 성공!");
+			JOptionPane.showMessageDialog(null, "MySQL.... Ok..!");
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ 510/src/DBTest.java
@@ -0,0 +1,7 @@
+
+public class DBTest {
+	public static void main(String[] args){
+		DBManage db = new DBManage();
+		db.DBSetting();
+	}
+}
